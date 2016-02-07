818ce64069b1fe85860b1c24e9261dbf42d6e81c
kyasu <kyasu.m2@gmail.com>
2016-02-07 11:47:30 +0900

    Remove init module

--- cm.mk
+++ cm.mk
@@ -5,3 +5,11 @@ $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
 PRODUCT_DEVICE := mondrianlte
 PRODUCT_NAME := cm_mondrianlte
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=mondrianlte \
+    PRODUCT_DEVICE=mondrianlte \
+    TARGET_DEVICE=mondrianlte \
+    PRODUCT_MODEL=SM-T325 \
+    BUILD_FINGERPRINT=samsung/mondrianltexx/mondrianlte:4.4.2/KOT49H/T325XXU1BNK2:user/release-keys \
+    PRIVATE_BUILD_DESC="mondrianltexx-user 4.4.2 KOT49H T325XXU1BNK2 release-keys"
